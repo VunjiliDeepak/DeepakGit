684735a8124b71515e0032e5c0ab750fff058f0e
VunjiliDeepak <vunjilideepak415@gmail.com>
2024-09-18 23:21:31 +0530

    Changed

--- tests/demo..spec.js
+++ tests/demo..spec.js
@@ -2,6 +2,7 @@ const{test,expect}= require('@playwright/test');
 
 test('has title', async ({ page }) => {
     await page.goto('https://parabank.parasoft.com/parabank/register.htm');
+    await expect(page).toHaveScreenshot();
   
    
   });
